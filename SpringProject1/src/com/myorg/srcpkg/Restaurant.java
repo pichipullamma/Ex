700e0a847b52e6a89f685ee52740636c3e97a30c
pav <pav@gmail.com>
2015-02-25 12:04:31 -0800

    first branch commit

--- SpringProject1/src/com/myorg/srcpkg/Restaurant.java
+++ SpringProject1/src/com/myorg/srcpkg/Restaurant.java
@@ -2,10 +2,22 @@ package com.myorg.srcpkg;
 
 public class Restaurant {
 
+	String welcomeNode;
 	
+	
+	public String getWelcomeNode() {
+		return welcomeNode;
+	}
+
+
+	public void setWelcomeNode(String welcomeNode) {
+		this.welcomeNode = welcomeNode;
+	}
+
+
 	public void greetCustomer(){
 		
-	System.out.println("welcome to spring !");	
+	System.out.println(welcomeNode);	
 	
 	}
 	
